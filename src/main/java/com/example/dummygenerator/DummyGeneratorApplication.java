9115dab0c7549eb760e0a3351f7a61dc38c3ee06
WonHwiAhn <myroom123@bible.ac.kr>
2020-12-08 00:34:33 +0900

    sample

---
+++ src/main/java/com/example/dummygenerator/DummyGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.example.dummygenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DummyGeneratorApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/dummygenerator/controller/AdminPageController.java
@@ -0,0 +1,24 @@
+package com.example.dummygenerator.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminPageController {
+    @RequestMapping({"/dashboard", "/"})
+    public String dashboardPage() {
+        return "admin/dashboard";
+    }
+
+    @GetMapping("/user/add")
+    public String addUser() {
+        return "admin/user-add";
+    }
+
+    @GetMapping({"/user", "/user/list"})
+    public String listUser() {
+        return "admin/user-list";
+    }
+}
